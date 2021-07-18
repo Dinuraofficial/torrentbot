76729a06af2c65a307410a7b562f46d8740a41a0
Dinuraofficial <87411492+Dinuraofficial@users.noreply.github.com>
2021-07-18 13:35:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ async def start_handler(_, message: Message):
         await message.reply_text(
             text="😋Hello, I am simple Torrent Search Bot!\n"
                  "✅I can search Torrent Magnetic Links from Inline.\n\n"
-                 "✅Made by @supunma ",
+                 "✅Made by @DinuraNikalansuriya",
             disable_web_page_preview=True,
             parse_mode="Markdown",
             reply_markup=InlineKeyboardMarkup(DEFAULT_SEARCH_MARKUP)
